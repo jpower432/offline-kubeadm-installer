0cf441583ee4555e618e3979838eccf828e9272d
Jennifer Power <jpower@alionscience.com>
2020-11-29 17:40:12 -0500

    Further configure containerd and kubelet for ARM and ARM64

--- ubuntu-arm/Dockerfile.groovy
+++ ubuntu-arm/Dockerfile.groovy
@@ -3,6 +3,7 @@ FROM ubuntu:groovy AS build-stage
 RUN mkdir -p /build/payload/images
 RUN mkdir /build/payload/packages
 RUN mkdir /build/payload/manifests
+RUN mkdir /build/payload/dependencies
 WORKDIR /build
 
 # Copy in images
@@ -27,6 +28,9 @@ RUN cp /var/cache/apt/archives/*.deb payload/packages/
 
 # Pull the weave-net manifest
 RUN curl https://cloud.weave.works/k8s/v1.16/net.yaml -o payload/manifests/weave-net.yaml
+
+# Pull crictl
+RUN curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/1.19.0/crictl-1.19.0-linux-arm.tar.gz --output payload/dependencies/crictl-1.19.0-linux-arm.tar.gz
 
 # Build it
 RUN bash build
